ecc1f140607de987f9d552a6ed53fb246ac2db24
frooh0 <128101961+frooh0@users.noreply.github.com>
2024-06-14 15:15:50 -0700

    Completed button functionality

--- Summer Game Jam 2024/Assets/Scripts/MenuManager.cs
+++ Summer Game Jam 2024/Assets/Scripts/MenuManager.cs
@@ -6,19 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    
+
+
     public void StartGame()
     {
+        //Debug.Log("StartGame method called");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
     public void EndGame()
     {
+        //Debug.Log("EndGame method called");
         Application.Quit();
     }
 
     public void RestartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-       
+        //Debug.Log("RestartGame method called");
+        SceneManager.LoadScene(0);
     }
 }
