fa51c31b9b3db844690fec0550488f160baee840
Justin Dela Cruz <128101961+jdelacruz1223@users.noreply.github.com>
2024-06-13 20:42:10 -0700

    UI Elements

---
+++ Summer Game Jam 2024/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void EndButton()
+    {
+        Application.Quit();
+    }
+
+    public void StartButton()
+    {
+
+    }
+    
+    public void ControlsButton()
+    {
+
+    }
+    
+    public void SettingsButton()
+    {
+        
+    }
+}
