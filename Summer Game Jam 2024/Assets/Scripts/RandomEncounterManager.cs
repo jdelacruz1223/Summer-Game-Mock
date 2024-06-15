a17952897ff0f4f0db146d36b96d8c931a0acb0f
dichill <dichilltomarong@yahoo.com>
2024-06-14 19:37:39 -0700

    Reworked Dialogue Manager as it wasn't working properly when turned into prefab. Also added a simple RandomEncounterManager

---
+++ Summer Game Jam 2024/Assets/Scripts/RandomEncounterManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    public class RandomEncounterManager : MonoBehaviour
+    {
+        [SerializeField] List<TextAsset> Stories;
+        public int secondsPerEncounter;
+        public float encounterPercentage;
+
+        public bool currentlyInEncounter { get; private set; }
+
+        private static RandomEncounterManager instance;
+
+        public static RandomEncounterManager GetInstance() { return instance; }
+        private void Awake()
+        {
+            if (instance != null) Debug.LogWarning("Another instance of the DialogueManager is running.");
+            instance = this;
+
+            StartCoroutine(RandomEncounter());
+        }
+
+        IEnumerator RandomEncounter()
+        {
+            yield return new WaitForSeconds(5);
+
+            // Select Randomly from the list of stories
+            int index = Random.Range(0, Stories.Count);
+            TextAsset inkJson = Stories[index];
+
+            DialogueManager.GetInstance().EnterEncounterDialogueMode(inkJson);
+        }
+    }
+}
