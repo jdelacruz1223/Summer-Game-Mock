d5e9ce31ffc7ccc97de36fba4d7e36f420a33c02
dichill <dichilltomarong@yahoo.com>
2024-06-15 19:01:15 -0700

    added a simple character setup

---
+++ Summer Game Jam 2024/Assets/Scripts/CharacterSetupManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CharacterSetupManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameTxt;
+    [SerializeField] private TMP_InputField partyTxt;
+    [SerializeField] private TextMeshProUGUI partyTextFormat;
+    [SerializeField] private GameObject partyListPanel;
+    public string username { get; private set; }
+    public string partyName { get; private set; }
+    public int totalMembers { get; private set; }
+
+    List<string> partyNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        partyNames = new List<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    IEnumerator RefreshPartyList()
+    {
+
+        float distBetweenText = 15f;
+        int index = 0;
+
+        foreach (Transform child in partyListPanel.transform)
+        {
+            if (child.gameObject.name != "TextFormat")
+                Destroy(child.gameObject);
+        }
+
+        yield return new WaitForEndOfFrame();
+
+        foreach (var name in partyNames)
+        {
+            Debug.Log(name);
+
+            TextMeshProUGUI text = Instantiate(partyTextFormat, partyListPanel.transform);
+            text.gameObject.SetActive(true);
+
+            text.text = name;
+
+            RectTransform rectTransform = text.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2(0, -distBetweenText * index);
+
+            index++;
+        }
+    }
+    public void AddToParty()
+    {
+        if (partyNames.Contains(partyName)) return;
+        if (partyTxt.text.Length == 0) return;
+
+        Manager.GetInstance().addToParty(partyName);
+        partyNames.Add(partyName);
+        StartCoroutine(RefreshPartyList());
+    }
+    public void RemoveFromParty(int x) {
+        partyNames.RemoveAt(x);
+        TextMeshProUGUI[] child = partyListPanel.GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (var c in child) Destroy(c);
+
+        StartCoroutine(RefreshPartyList());
+        Manager.GetInstance().removeToParty(name);
+    }
+
+    public void FinishCharacterSetup()
+    {
+
+    }
+
+    public void setName() => username = nameTxt.text;
+    public void setPartyName() => partyName = partyTxt.text;
+}
