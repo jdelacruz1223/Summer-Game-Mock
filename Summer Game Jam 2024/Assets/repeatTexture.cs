860755a50b2ff65def30394846af91fc7722e6c5
frooh0 <128101961+frooh0@users.noreply.github.com>
2024-06-18 17:38:15 -0700

    quad road texture

---
+++ Summer Game Jam 2024/Assets/repeatTexture.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repeatTexture : MonoBehaviour
+{
+    public float speed;
+    Vector2 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(0,Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
