6bbe6fb817290508371632f907257f982a696d54
toonlank <50122169+toonlank@users.noreply.github.com>
2024-07-17 16:49:59 -0700

    added fire rate and accuracy, and integrated debug manager

--- Summer Game Jam 2024/Assets/Scripts/ShootingGame/targetScript.cs
+++ Summer Game Jam 2024/Assets/Scripts/ShootingGame/targetScript.cs
@@ -8,7 +8,15 @@ using UnityEngine.InputSystem.Controls;
 public class targetScript : MonoBehaviour
 {
     public Camera gameCamera;
+    public GameObject manager;
+    [Range(0,10)]
+    public float fireRate = 1f;
+    private float nextFire;
+    private int shotsFired;
+    private int shotsHit;
+    private float Accuracy;
     private GameObject bottle;
+    private DebugManager debugManager;
     private Vector3 target;
     private Vector3 mousePos;
     //public GameObject CrossHairs;
@@ -17,6 +25,10 @@ public class targetScript : MonoBehaviour
     void Start()
     {
         //Cursor.visible = false;
+        shotsFired = 0;
+        shotsHit = 0;
+        debugManager = manager.GetComponent<DebugManager>();
+        debugManager.setBottleDestroyedCount(0);
     }
 
     // Update is called once per frame
@@ -25,20 +37,25 @@ public class targetScript : MonoBehaviour
         mousePos = Input.mousePosition; 
         Ray ray = gameCamera.ScreenPointToRay(mousePos);
         //target = gameCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, gameCamera.nearClipPlane));
-        if (Input.GetMouseButtonDown(0)) {
-            Debug.DrawRay(target, ray.direction * 100, Color.red, 2f);
+        if (Input.GetMouseButtonDown(0) && Time.time > nextFire) {
+            shotsFired++;
+            nextFire = Time.time + fireRate;
+            Debug.DrawRay(gameCamera.transform.position, ray.direction * 100, Color.red, 2f);
             //Debug.Log("Mouse position was at (" + target.x + ", " + target.y + ", " + target.z +")");
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
                 bottle = hit.collider.gameObject;
                 if (bottle.CompareTag("Bottle")) {
+                    shotsHit++;
+                    debugManager.increaseBottleDestroyedCount(1);
                     bottle.SetActive(false);
                     Debug.Log("A bottle has been hit!");
-                } 
+                }
             } else {
                 Debug.Log("Miss!");
             }
-            
+            Accuracy = (shotsFired != 0) ? (float) shotsHit / shotsFired : 0f;
+            Debug.Log("Accuracy: " + Accuracy * 100 + "%"); 
         }
     }
 }
